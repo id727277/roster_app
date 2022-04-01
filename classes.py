528d72b011e8cf54644b1d736d405cc3976a2984
Pavel Komar <pasha.p.komar@gmail.com>
2022-04-01 15:12:49 +0300

    started the project

---
+++ classes.py
@@ -0,0 +1,25 @@
+class Employee:
+    ''' This class will contain general employee attributes and methods
+    
+    additional general attributes:
+    - Sick leave
+    - Vacation
+    - Training
+    '''
+
+    def __init__(self, first, last, mob_phone, dept):
+        self.first = first
+        self.last = last
+        self.mob_phone = mob_phone
+        self.dept = dept
+
+
+'''
+additional attributes for concierge:
+
+    tg_account
+    workstation
+    nights=False
+
+
+'''
